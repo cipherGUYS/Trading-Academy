f7c787110e2edd93a96403c636ba8f10dfcaa8e2
Tomin-Joy <tominjk007@gmail.com>
2022-07-10 04:02:19 +0530

    const formData = document.forms['Regform']
    var NAME = formData['Email'].value
    var PASS= formData['Password'].value
    
    console.log(formData)

---
+++ back-script.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  get,
+  set,
+  child,
+  update,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.6.3/firebase-database.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyAXpdLMnPpeXo-LgpW-94orWVhrS4EGN-0",
+  authDomain: "slash-key.firebaseapp.com",
+  databaseURL: "https://slash-key-default-rtdb.firebaseio.com",
+  projectId: "slash-key",
+  storageBucket: "slash-key.appspot.com",
+  messagingSenderId: "593448539333",
+  appId: "1:593448539333:web:fd68e4d6c17f208bcc0cac",
+  measurementId: "G-2QZGFD4552",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+function add() {
+  
+  const formData = document.forms["Regform"];
+  var NAME = formData["Email"].value;
+  var PASS = formData["Password"].value;
+
+  update(ref(db, "Student/"), {
+    NAME: {
+      name: NAME,
+      pass: PASS,
+    },
+  }).then(() => {
+    window.location.href='/index.html';
+  });
+}
+document.getElementById("submit").addEventListener("click", () => {
+  add();
+});
+
+function funda() {
+  console.log("Akash Funda");
+}
